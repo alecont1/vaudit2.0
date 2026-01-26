0c5575cfc45c00fc097e773f495d0a46554d2dec
alecont1 <alecont1@users.noreply.github.com>
2026-01-26 19:47:09 -0300

    chore(03-02): export new validators from package
    - Add validate_calibration to package exports
    - Add validate_serial_consistency to package exports
    - Add collect_serial_numbers to package exports
    - Enables clean imports: from src.domain.validators import validate_calibration

--- src/domain/validators/__init__.py
+++ src/domain/validators/__init__.py
@@ -1,5 +1,14 @@
 """Validation utilities for AuditEng V2."""
 
+from src.domain.validators.calibration import validate_calibration
 from src.domain.validators.date_parser import DateFormat, detect_format, parse_date
+from src.domain.validators.serial import collect_serial_numbers, validate_serial_consistency
 
-__all__ = ["parse_date", "DateFormat", "detect_format"]
+__all__ = [
+    "parse_date",
+    "detect_format",
+    "DateFormat",
+    "validate_calibration",
+    "validate_serial_consistency",
+    "collect_serial_numbers",
+]
